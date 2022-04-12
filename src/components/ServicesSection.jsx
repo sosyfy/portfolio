e070a4e4b6f8ec185931a900b31c25c335223761
Sospeter Karanja <98221088+sosyfy@users.noreply.github.com>
2022-04-12 10:52:52 +0300

    updated services section

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -1,38 +1,47 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Service =styled.div`
-    dispay:flex;
-    flex-direction: column;
-    text-align: center;
-    gap:10rem;
+const Service = styled.div`
+  dispay: flex;
+  flex-direction: column;
+  text-align: center;
+  gap: 10rem;
+  position: relative;
+  box-shadow: 0 0 10px whitesmoke;
+  border-radius: 10px;
+  padding: 3rem 1rem;
+  // border: 1px solid whitesmoke;
 
-.service-title h2{
-  font-size: 2.5rem;
-  font-family: 'montserrat ';
-  color: cyan;
-}
-.service-text {
-   padding: 1rem;
-   margin-top: 2rem;
-   line-height: 2rem;
-   font-size: 1.3rem;
-}
-.service-icon svg {
+
+  &:hover {
+    border: 1px solid aqua;
+  }
+
+  .service-title h2 {
+    font-size: 2.5rem;
+    font-family: "montserrat ";
+    color: cyan;
+  }
+  .service-text {
+    padding: 1rem;
+    margin-top: 2rem;
+    line-height: 2rem;
+    font-size: 1.3rem;
+  }
+  .service-icon svg {
     width: 3rem;
-}
-@media screen and (max-width: 768px){
-    padding: 0;
+  }
+  @media screen and (max-width: 768px) {
+    padding: 1.5rem 1rem;
+
     .service-title {
-        font-size: 1rem ;
+      font-size: 1rem;
     }
     .service-text {
-        padding:0;
+      padding: 0;
     }
-
-}
-
-`
+  }
+`;
 function ServicesSection({icon, title , text}) {
   return (
     <Service data-aos="zoom-in-up" data-aos-duration="2000">
